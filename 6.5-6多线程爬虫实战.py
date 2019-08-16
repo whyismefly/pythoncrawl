2f4e0cb191384f3dba8a70e97400fc67a0784567
whyismefly <whyismeflying@hotmail.com>
2019-08-16 17:43:32 +0800

    20190816-1

--- 6.5-6多线程爬虫实战.py
+++ 6.5-6多线程爬虫实战.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
 # encoding:utf-8
 
+import threading
+class A (threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        for i in range(10):
+            print("aaaaaaaaa"+str(i))
+class B (threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        for i in range(10):
+            print("bbbbbbbbbb"+str(i))
+
+t1=A()
+t1.start()
+t2=B()
+t2.start()
