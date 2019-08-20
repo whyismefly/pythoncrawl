5a93b9cb77a26e8741ab43b6fe1f36ff938bd1e5
whyismefly <whyismeflying@hotmail.com>
2019-08-20 10:17:47 +0800

    20190820

--- 6.5-6多线程爬虫实战.py
+++ 6.5-6多线程爬虫实战.py
@@ -2,6 +2,12 @@
 # encoding:utf-8
 
 import threading
+import queue
+import re
+import urllib.request
+import time
+import urllib.error
+
 class A (threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
@@ -15,7 +21,62 @@ class B (threading.Thread):
         for i in range(10):
             print("bbbbbbbbbb"+str(i))
 
-t1=A()
-t1.start()
-t2=B()
-t2.start()
+# t1=A()
+# t1.start()
+# t2=B()
+# t2.start()
+
+a=queue.Queue()
+a.put("hello")
+a.task_done()
+print(a.get())
+a.put("python")
+a.task_done()
+a.put("php")
+a.task_done()
+a.put("java")
+a.task_done()
+print(a.get())
+print(a.get())
+print(a.get())
+
+urlqueue=queue.Queue()
+headers=(
+"user-agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
+)
+opener=urllib.request.build_opener()
+opener.addheaders=[headers]
+urllib.request.install_opener(opener)
+listurl=[]
+def use_proxy(proxy_addr,url):
+    try:
+        proxy=urllib.request.ProxyHandler({'http':proxy_addr})
+        opener=urllib.request.build_opener(proxy,urllib.request.HTTPHandler)
+        urllib.request.install_opener(opener)
+        data=urllib.request.urlopen(url).read().decode('utf-8')
+        return data
+    except urllib.error.URLError as e:
+        if hasattr(e,"code"):
+            print(e.code)
+        if hasattr(e,"reason"):
+            print(e.reason)
+        time.sleep(10)
+    except Exception as e:
+        print("excption:"+str(e))
+        time.sleep(1)
+
+class geturl(threading.Thread):
+    def __init__(self,key,pagestart,pageend,proxy,urlqueue):
+        threading.Thread.__init__(self)
+        self.pagestart=pagestart
+        self.pageend=pageend
+        self.proxy=proxy
+        self.urlqueue=urlqueue
+        self.key=key
+    def run(self):
+        page=self.pagestart
+        keycode=urllib.request.quote("key")
+        pagecode=urllib.request.quote("&queue")
+        for page in range(self.pagestart,self.pageend+1):
+            url="http://weixin.sougou.com/weixin?type=2&query="+keycode+pagecode+str(page)
+            data1=use_proxy(self.proxy,url)
