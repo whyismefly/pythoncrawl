f399087dad92c6f30e725ac8188117bc29bb5462
whyismefly <whyismeflying@hotmail.com>
2020-01-14 11:55:18 +0800

    2020-01-14
    scrapy

---
+++ 12.1-12开启scrapy爬虫项目项目之旅.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# encoding:utf-8
+
+# from scrapy.spiders import Spider
+#
+# class FirstSpider(Spider):
+#     name = "first"
+#     allowed_domains = ["baidu.com"]
+#     start_urls = [
+#         "http://www.baidu.com",
+#     ]
+#
+# def parse(self,response):
+#     pass
+
+# >>> ti=sel.xpath("/html/head/title")
+# >>> print(ti)
+# [<Selector xpath='/html/head/title' data='<title>百度一下，你就知道</title>'>]
+# >>>
+
+# >>> exit()
+# D:\Python35\myweb\part12>
+
+#12.4 实战Items
+# class MyfirstpjtItem(scrapy.Item):
+# #     # define the fields for your item here like:
+# #     # name = scrapy.Field()
+# #     urlname=scrapy.Field()
+# #     urlkey=scrapy.Field()
+# #     urlcr=scrapy.Field()
+# #     urladdr=scrapy.Field()
+
+
+
