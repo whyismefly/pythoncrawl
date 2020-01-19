f49af5761d7bdaa8f495d841c2cc7792eb9abe5e
whyismefly <whyismeflying@hotmail.com>
2020-01-19 17:30:10 +0800

    20200119-1

---
+++ myfirstpjt/autopjt/autopjt/spiders/autospd.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from autopjt.items import AutopjtItem
+
+class AutospdSpider(scrapy.Spider):
+    name = 'autospd'
+    allowed_domains = ['dangdang.com']
+    start_urls = ['http://category.dangdang.com/pg1-cid4002203.html',]
+
+    def parse(self, response):
+        # pass
+        item = AutopjtItem()
+        item["name"] = response.xpath("//a[@class='pic']/@title").extract()
+        item["price"] = response.xpath("//span[@class='price_n']/text()").extract()
+        item["link"] = response.xpath("//a[@class='pic']/@href").extract()
+        item["comnum"] = response.xpath("//a[@name='P_pl']/text()").extract()
+        yield item
+        for i in range(1,76):
+            url="http://category.dangdang.com/pg"+str(i)+"-cid4002203.html"
+            yield Request(url,callback=self.parse)
