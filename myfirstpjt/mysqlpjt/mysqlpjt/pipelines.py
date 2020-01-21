dea63b36482e752530ad3d5247cd54225588ba3e
whyismefly <whyismeflying@hotmail.com>
2020-01-21 11:47:05 +0800

    Update pipelines.py

--- myfirstpjt/mysqlpjt/mysqlpjt/pipelines.py
+++ myfirstpjt/mysqlpjt/mysqlpjt/pipelines.py
@@ -11,7 +11,16 @@ class MysqlpjtPipeline(object):
 
     def __init__(self):
         self.conn=pymysql.connect(host="127.0.0.1", user="root", passwd="root", db="mypydb", charset='utf8')
-        #数据库想存汉字一定要设置为nchar，不要用书里面的char，也可以直接在连接中设置charset为utf8（不是utf-8）
+        #数据库想存汉字一定要设置为nchar，不要用书里面的char，也可以直接在连接中设置charset为utf8（不是utf-8），这样就不要
+        # 改pymysql下面的connections.py了，具体改为：
+        # def __init__(self, host="localhost", user=None, passwd="",
+        #              db=None, port=3306, unix_socket=None,
+        #              charset='utf8', sql_mode=None,
+        #              read_default_file=None, conv=decoders, use_unicode=None,
+        #              client_flag=0, cursorclass=Cursor, init_command=None,
+        #              connect_timeout=None, ssl=None, read_default_group=None,
+        #              compress=None, named_pipe=None):
+
     def process_item(self, item, spider):
         name = str(item["name"][0])
         key = str(item["keywd"][0])
