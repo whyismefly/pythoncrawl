fdd7c989ca47b05c822c5da067797aa08faf435b
whyismefly <whyismeflying@hotmail.com>
2020-04-26 08:52:15 +0800

    20200426

---
+++ 网络爬虫实战/第三章.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# encoding:utf-8
+
+import http.cookiejar,urllib.request
+
+# cookie = http.cookiejar.CookieJar()
+# handler = urllib.request.HTTPCookieProcessor(cookie)
+# opener = urllib.request.build_opener(handler)
+# response = opener.open("http://www.baidu.com")
+# for item in cookie:
+#     print(item.name+"="+item.value)
+filename = "cookies.txt"
+cookie = http.cookiejar.MozillaCookieJar(filename)
+handler = urllib.request.HTTPCookieProcessor(cookie)
+opener = urllib.request.build_opener(handler)
+response = opener.open("http://weibo.com/")
+cookie.save(ignore_discard = True, ignore_expires = True)
