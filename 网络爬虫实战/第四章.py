500e6045404e3b9fb23bbf9e1059611dd2570f3b
whyismefly <whyismeflying@hotmail.com>
2020-05-26 10:18:09 +0800

    20200526

--- 网络爬虫实战/第四章.py
+++ 网络爬虫实战/第四章.py
@@ -2,7 +2,8 @@
 # encoding:utf-8
 
 from lxml import etree
-text='''
+
+text = '''
 <div>
     <ul>
          <li class="item-0"><a href="link1.html">first item</a></li>
@@ -13,7 +14,71 @@ text='''
      </ul>
  </div>
 '''
-html=etree.HTML(text)
-result=etree.tostring(html)
-print(result.decode('utf-8'))
+html = etree.HTML(text)
+# result=etree.tostring(html)
+# print(result.decode('utf-8'))
+
+# result = html.xpath('//li/a[1]/text()')
+# print(result)
+# result = html.xpath('//li[1]/a/text()')
+# print(result)
+# result = html.xpath('//li[last()]/a/text()')
+# print(result)
+# result = html.xpath('//li[position()<3]/a/text()')
+# print(result)
+# result = html.xpath('//li[last()-2]/a/text()')
+# print(result)
+
+# from bs4 import BeautifulSoup
+# soup = BeautifulSoup('<p>Hello</p>','lxml')
+# print(soup.p.string)
+
+html = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title" name="dromouse"><b>The Dormouse's story</b></p>
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1"><!-- Elsie --></a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+<p class="story">...</p>
+"""
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html, 'lxml')
+# print(soup.prettify())
+# print(soup.title.string)
+# print(soup.title)
+# print(type(soup.title))
+# print(soup.title.string)
+# print(soup.head)
+# print(soup.p)
+# print(soup.title.name)
+# print(soup.p.attrs)
+# print(soup.p.attrs['name'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
