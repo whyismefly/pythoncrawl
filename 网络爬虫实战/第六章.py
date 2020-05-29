f6cf0278938bbde6ada6fd97be7c006a2aca02f3
whyismefly <whyismeflying@hotmail.com>
2020-05-29 21:51:10 +0800

    Update 第六章.py

--- 网络爬虫实战/第六章.py
+++ 网络爬虫实战/第六章.py
@@ -64,41 +64,74 @@ import requests
 from urllib.parse import urlencode
 from hashlib import md5
 import os
+from multiprocessing.pool import Pool
+
 
 def get_page(offset):
-    params={
-        'offset':offset,
-        'format':'json',
-        'keyword':'街拍',
-        'autoload':'true',
-        'count':'20',
-        'cur_tab':'1',
+    params = {
+        'offset': offset,
+        'format': 'json',
+        'keyword': '街拍',
+        'autoload': 'true',
+        'count': '20',
+        'cur_tab': '1',
     }
-    url='http://www.toutiao.com/search_content/?'+urlencode(params)
+    url = 'http://www.toutiao.com/search_content/?' + urlencode(params)
     try:
-        response=requests.get(url)
-        if response.status_code==200:
+        response = requests.get(url)
+        if response.status_code == 200:
             return response.json()
     except requests.ConnectionError:
         return None
 
+
 def get_images(json):
     if json.get('data'):
-        title = item.get('data')
-        images = item.get('title')
-        for image in images:
-            yield {
-                'image':image.get('url'),
-                'title':title
-            }
+        for item in json.get('data'):
+            title = item.get('title')
+            images = item.get('image_detail')
+            for image in images:
+                yield {
+                    'image': image.get('url'),
+                    'title': title
+                }
+
 
 def save_image(item):
     if not os.path.exists(item.get('title')):
         os.mkdir(item.get('title'))
     try:
-        response=requests.get(item.get('image'))
-        if response.status_code==200:
-            file_path=''
+        response = requests.get(item.get('image'))
+        if response.status_code == 200:
+            file_path = '{0}/{1}.{2}'.format(item.get('title'), md5(response.content).hexdigest(), 'jpg')
+            if not os.path.exists(file_path):
+                with open(file_path, 'wb')as f:
+                    f.write(response.content)
+            else:
+                print('downloaded', file_path)
+    except requests.ConnectionError:
+        print('failed')
+
+
+def main(offset):
+    json = get_page(offset)
+    for item in get_images(json):
+        print(item)
+        save_image(item)
+
+
+GROUP_START = 1
+GROUP_END = 20
+
+if __name__ == '__main__':
+    pool = Pool()
+    groups = ([x * 20 for x in range(GROUP_START, GROUP_END + 1)])
+    pool.map(main, groups)
+    pool.close()
+    pool.join()
+
+
+
 
 
 
